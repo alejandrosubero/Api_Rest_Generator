7ff0fa12a65e0e42053264b2f8b909c60418c418
Alejandro Subero <alejandrosubero.ar@gmail.com>
2024-04-12 11:54:10 -0400

    add old apirest proyect

--- apirest/src/main/java/com/Generator/apirest/services/ValidateService.java
+++
@@ -1,8 +0,0 @@
-package com.Generator.apirest.services;
-
-
-import com.Generator.apirest.pojos.master.ArchivoBaseDatosPojo;
-
-public interface ValidateService {
-	public boolean validateSaveProyecte(ArchivoBaseDatosPojo archivo);
-}
